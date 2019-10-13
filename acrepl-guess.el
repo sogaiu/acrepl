ddf1795c041317fe428fee27489d06f2cdaf3fc5
sogaiu <983021772@users.noreply.github.com>
2019-10-13 17:46:08 +0900

    Make guess-repl-buffer work from repl-buffers

--- acrepl-guess.el
+++ acrepl-guess.el
@@ -9,10 +9,16 @@
 (require 'acrepl-state)
 
 (defun acrepl-guess-repl-buffer ()
-  "Return associated repl buffer for the current code buffer.
-Should be executed in the context of a code buffer."
-  (let* ((code-path (buffer-file-name))
-         (repl-buffer (acrepl-get-repl-buffer code-path)))
+  "Return relevant repl-buffer.
+If current buffer is a repl-buffer, return that.
+Otherwise, if current buffer is a code buffer, try to find an associated
+repl-buffer.
+Otherwise, return nil."
+  (let* ((buffer-name (buffer-name))
+         (repl-buffer (if (acrepl-repl-buffer-name? buffer-name)
+                          (current-buffer)
+                        (let ((code-path (buffer-file-name)))
+                          (acrepl-get-repl-buffer code-path)))))
     (when (not repl-buffer)
       (with-temp-message
           "Did not find repl buffer.  Looking for alternative..."
