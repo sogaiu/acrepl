22ba63eb05fe391519fefa0ef1371d50dcbb6e6a
sogaiu <983021772@users.noreply.github.com>
2019-10-14 08:14:56 +0900

    Fix for guessing repl-buffer

--- acrepl-guess.el
+++ acrepl-guess.el
@@ -8,23 +8,27 @@
 
 (require 'acrepl-state)
 
+(defun acrepl-guess-helper ()
+  "Helper function for guessing repl-buffer."
+  (let ((buffer-name (buffer-name)))
+    (if (acrepl-repl-buffer-name? buffer-name)
+        (current-buffer)
+      (when-let ((code-path (buffer-file-name)))
+        (acrepl-get-repl-buffer code-path)))))
+
 (defun acrepl-guess-repl-buffer ()
   "Return relevant repl-buffer.
 If current buffer is a repl-buffer, return that.
 Otherwise, if current buffer is a code buffer, try to find an associated
 repl-buffer.
 Otherwise, return nil."
-  (let* ((buffer-name (buffer-name))
-         (repl-buffer (if (acrepl-repl-buffer-name? buffer-name)
-                          (current-buffer)
-                        (let ((code-path (buffer-file-name)))
-                          (acrepl-get-repl-buffer code-path)))))
+  (let (repl-buffer (acrepl-guess-helper))
     (when (not repl-buffer)
       (with-temp-message
           "Did not find repl buffer.  Looking for alternative..."
         (run-hooks 'acrepl-guess-repl-hook))
       ;; now try again and take whatever comes back
-      (setq repl-buffer (acrepl-get-repl-buffer code-path)))
+      (setq repl-buffer (acrepl-guess-helper)))
     repl-buffer))
 
 (provide 'acrepl-guess)
